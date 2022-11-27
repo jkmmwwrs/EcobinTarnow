6010c85f1e840ed84ee64801b19cfaa601cf1af4
JcobV01 <jakubwadycki@gmail.com>
2022-11-27 09:43:02 +0100

    QR 2

---
+++ app/src/main/java/com/example/ecobintarnow/DatabaseRow.kt
@@ -0,0 +1,7 @@
+package com.example.testapp
+
+data class DatabaseRow(
+    val userID: String,
+    val email: String,
+    val points: Int?
+)
--- app/src/main/java/com/example/ecobintarnow/QR.kt
+++ app/src/main/java/com/example/ecobintarnow/QR.kt
@@ -1,11 +1,99 @@
 package com.example.ecobintarnow
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.view.View.inflate
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.budiyev.android.codescanner.*
+import com.example.ecobintarnow.R.*
+import com.example.ecobintarnow.databinding.ActivityQrBinding
+import com.example.testapp.DatabaseRow
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
 
 class QR : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    private lateinit var codeScanner: CodeScanner
+    private lateinit var binding: ActivityQrBinding
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var myRef: DatabaseReference
+    val mDatabase = FirebaseDatabase.getInstance().getReference("ArrayData");
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_qr)
+        binding = ActivityQrBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val btnBack = binding.btnBack
+//        val textField = binding.textView
+
+        btnBack.setOnClickListener(View.OnClickListener { startActivity(Intent(this, SecondActivity::class.java)) })
+
+        val scannerView = binding.scannerView
+
+        codeScanner = CodeScanner(this, scannerView)
+        codeScanner.camera = CodeScanner.CAMERA_BACK
+        codeScanner.formats = CodeScanner.ALL_FORMATS
+        codeScanner.autoFocusMode = AutoFocusMode.SAFE
+        codeScanner.scanMode = ScanMode.SINGLE
+        codeScanner.isAutoFocusEnabled = true
+        codeScanner.isFlashEnabled = false
+
+        auth = Firebase.auth
+
+
+        codeScanner.decodeCallback = DecodeCallback {
+            runOnUiThread {
+                //Toast.makeText(this, "Scan result: ${it.text}", Toast.LENGTH_LONG).show()
+                val userID = auth.currentUser?.uid.toString()
+                val email = auth.currentUser?.email.toString()
+
+                val points = intent.getStringExtra("PKT_DATA")?.toInt()?.plus(5)
+                val firebase = FirebaseDatabase.getInstance()
+                val FBInput = DatabaseRow(userID, email, points)
+
+                myRef = firebase.getReference("ArrayData")
+                myRef.child(userID).setValue(FBInput)
+
+
+//                textField.text = "Zczytany kod: ${it.text}"
+
+                val explicitIntent = Intent(applicationContext, SecondActivity::class.java)
+                explicitIntent.putExtra("QR_DATA", it.text)
+                startActivity(explicitIntent)
+            }
+        }
+        codeScanner.errorCallback = ErrorCallback {
+            runOnUiThread {
+                Toast.makeText(
+                    this, "Błąd wczytywania kamery: ${it.message}",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+
+        scannerView.setOnClickListener {
+            codeScanner.startPreview()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        codeScanner.startPreview()
+    }
+
+    override fun onPause() {
+        codeScanner.releaseResources()
+        super.onPause()
     }
 }
+
+
