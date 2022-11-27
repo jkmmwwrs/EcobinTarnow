e9c4feac2a0a26200cdbe2c38812d33a9b09ecf0
Marcin <tarnowos.ecobin@gmail.com>
2022-11-27 09:58:32 +0100

    interface for main app

--- app/src/main/java/com/example/ecobintarnow/SecondActivity.kt
+++ app/src/main/java/com/example/ecobintarnow/SecondActivity.kt
@@ -2,10 +2,16 @@ package com.example.ecobintarnow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ecobintarnow.databinding.ActivityMainBinding
+import com.example.ecobintarnow.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySecondBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
