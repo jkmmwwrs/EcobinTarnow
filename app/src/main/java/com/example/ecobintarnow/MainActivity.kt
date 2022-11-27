b0cab9fce13e488b61238c63e7fbd5abcdf4cbe6
Marcin <tarnowos.ecobin@gmail.com>
2022-11-27 09:44:42 +0100

    interface

--- app/src/main/java/com/example/ecobintarnow/MainActivity.kt
+++ app/src/main/java/com/example/ecobintarnow/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.ecobintarnow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ecobintarnow.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
