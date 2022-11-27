8b150eed04be61dd122c3ed46befc25b13c8752e
Marcin <tarnowos.ecobin@gmail.com>
2022-11-27 09:02:06 +0100

    qr add

---
+++ app/src/main/java/com/example/ecobintarnow/QR.kt
@@ -0,0 +1,11 @@
+package com.example.ecobintarnow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QR : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_qr)
+    }
+}
